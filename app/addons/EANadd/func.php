d7b029bb6ae6539b2228b26da0d259c70a12d37a
Satamael <demidvip@gmail.com>
2019-06-30 21:53:51 +0400

    Relise

--- app/addons/EANadd/func.php
+++ app/addons/EANadd/func.php
@@ -1,6 +1,7 @@
 <?php
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
-function fn_EANadd_detailed_content(){
+function fn_EANadd_detailed_content()
+{
 	 $arow = db_query("UPDATE ?:products SET ?u WHERE product_id = ?i", $_data, $product_id);
 }
